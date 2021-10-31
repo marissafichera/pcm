153709a2cd6fee939822e553cf4abeecaeb81c86
Jake Ross <jirhiker@gmail.com>
2021-10-31 07:55:55 -0600

    0.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pychron-cm",
-    version="0.2.0",
+    version="0.2.1",
     author="Jake Ross",
     description="Pychron configuration manager",
     long_description=long_description,
