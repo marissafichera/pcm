7f2b8f29fb363f82d653423b4860b1759a73c357
Jake Ross <jirhiker@gmail.com>
2022-06-16 10:13:02 -0600

    bummed version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pychron-cm",
-    version="0.3.14",
+    version="0.3.15",
     author="Jake Ross",
     description="Pychron configuration manager",
     long_description=long_description,
